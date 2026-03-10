4a1aa17f6c385945fca0b35eb914869e917f3677
Mattia Verardi <verardi.mattia97@gmail.com>
2026-03-10 09:35:28 +0100

    Create database script

---
+++ sql/01_create_database.sql
@@ -0,0 +1,10 @@
+/*
+Project: Online Retail SQL Analysis
+File: 01_create_database.sql
+Description: Script per creare il database del progetto
+Author: Mattia Verardi
+Date: March 2026
+*/
+
+create database OnlineRetailDB;
+go
